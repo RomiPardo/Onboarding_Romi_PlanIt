e25c0f853a4353a826c2ca6568ed93701c16aca2
YOUR NAME <YOUR EMAIL>
2023-10-20 13:49:56 -0300

    feat: prisma seed

---
+++ prisma/seed.ts
@@ -0,0 +1,34 @@
+import prisma from "~/server/db";
+
+async function main() {
+  const alice = await prisma.user.upsert({
+    where: { email: 'alice@prisma.io' },
+    update: {},
+    create: {
+      email: 'alice@prisma.io',
+      name: 'Alice',
+      lastName: 'Joy',
+      business: 'star',
+    },
+  })
+  const bob = await prisma.user.upsert({
+    where: { email: 'bob@prisma.io' },
+    update: {},
+    create: {
+      email: 'bob@prisma.io',
+      name: 'Bob',
+      lastName: 'Ray',
+      business: 'logans',
+    },
+  })
+  console.log({ alice, bob })
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
