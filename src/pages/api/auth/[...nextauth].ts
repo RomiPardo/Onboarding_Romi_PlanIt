27a3a3bbff141ac8c0043cce5eb1c230f91951d7
YOUR NAME <YOUR EMAIL>
2023-10-20 12:32:11 -0300

    fix: next auth secret and deletion of discord client

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,4 +2,10 @@ import NextAuth from "next-auth";
 
 import { authOptions } from "~/server/auth";
 
-export default NextAuth(authOptions);
+export default NextAuth({
+  providers: [
+  ],
+  callbacks: {
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+});
