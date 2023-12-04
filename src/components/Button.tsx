febfcc47aa74de2547d76458abd07950845d75c8
YOUR NAME <YOUR EMAIL>
2023-12-04 11:40:48 -0300

    feat: confirmation page

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,9 @@ const buttonStyles = cva("", {
       primary:
         "box-border h-8 w-full rounded border-0 bg-gradient-to-br from-blue-300 to-blue-500 text-center text-base font-medium leading-4 text-white shadow-sm hover:cursor-pointer md:h-9 md:text-base md:font-medium",
       secondary:
-        "box-border h-8 w-full rounded border-1 bg-wite text-center text-base font-medium leading-4 text-gray shadow-sm hover:cursor-pointer md:h-9 md:text-base md:font-medium",
+        "box-border h-8 w-full rounded border border-gray bg-wite text-center text-base font-medium leading-4 text-gray shadow-sm hover:cursor-pointer md:h-9 md:text-base md:font-medium",
+      terciary:
+        "box-border h-8 w-full rounded border bg-wite text-center text-base font-medium leading-4 text-black shadow-sm hover:cursor-pointer md:h-9 md:text-base md:font-medium border-black",
       help: "flex h-8 w-56 flex-row items-center justify-center gap-3 rounded-xl bg-blue-300 text-base font-normal leading-4 text-white",
       edition: "ms:text-right h-9 text-left hover:cursor-pointer",
     },
