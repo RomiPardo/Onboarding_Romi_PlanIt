a12f0600a08b36c6b767a02b1108580498d58800
YOUR NAME <YOUR EMAIL>
2023-12-15 10:47:00 -0300

    fix: gitignore problem

--- mail-information.ts
+++ mail-information.ts
@@ -1,2 +1,14 @@
-export const EMAIL_ADDRESS = "aux5687@gmail.com";
-export const EMAIL_PASSWORD = "thzp dfsz gyze jjba";
+export const EMAIL_ADDRESS = "your mail";
+export const EMAIL_PASSWORD = "your app password";
+
+// Steps to get your email infromation:
+
+// 1 - Make a Google account and go to configuration.
+
+// 2 - Enable 2-Step Verification in your Google account.
+
+// 3 - Search in the configuration browser "App password"
+
+// 4 - Generate a app password
+
+// 5 - When you get this data, complete the above
