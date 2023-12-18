9fe08f56a64112d5c3596429fd40b68f31abf87b
YOUR NAME <YOUR EMAIL>
2023-12-18 10:23:41 -0300

    chore: modifications in email design

--- src/nodemailer/email.tsx
+++ src/nodemailer/email.tsx
@@ -5,6 +5,9 @@ import {
   Body,
   Tailwind,
   Container,
+  Img,
+  Head,
+  Section,
 } from "@react-email/components";
 
 type EmailProps = {
@@ -17,16 +20,32 @@ export const Email = ({ message }: EmailProps) => {
   return (
     <Html>
       <Tailwind>
-        <Container className="bg-blue-300 text-black">
-          <Body className="p-2">
-            <div className="rounded-lg border border-black bg-white px-6 py-2">
+        <Container className="w-full bg-[#FBFBFB] text-black">
+          <Head>
+            <Img
+              src="https://www.planit.com.uy/assets/logo/logo_black-c95d6135932d06deef913ba23f6b5cbef09f86e72d84772ceb8cc0cb2efb5be1.png"
+              alt="Planit Logo"
+              width="96"
+              height="33.47"
+              className="pb-4 pl-5 pt-9"
+            />
+          </Head>
+
+          <Body className="px-5 py-5">
+            <Section className="border border-[#7D7D7D] bg-white px-6 py-2">
               {message.split(matchLineBreak).map((paragraph, index) => (
                 <Text className="text-lg font-normal" key={index}>
                   {paragraph}
                 </Text>
               ))}
-            </div>
+            </Section>
           </Body>
+
+          <Container className="pb-5">
+            <Text className="text-center text-dark-gray">
+              PlanIT, 2021 - All rights reserved
+            </Text>
+          </Container>
         </Container>
       </Tailwind>
     </Html>
