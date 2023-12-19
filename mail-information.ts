3c774de36c3c33461154007ac4814f8e751f2d72
YOUR NAME <YOUR EMAIL>
2023-12-19 14:46:09 -0300

    chore: delete mail information data

--- mail-information.ts
+++ mail-information.ts
@@ -1,5 +1,5 @@
-export const EMAIL_ADDRESS = "aux5687@gmail.com";
-export const EMAIL_PASSWORD = "thzp dfsz gyze jjba";
+export const EMAIL_ADDRESS = "your email";
+export const EMAIL_PASSWORD = "your app password";
 
 // Steps to get your email infromation:
 
